1f630c97b2d2f1650b111d794878566baad6e5e2
morgoe <morgan.carter@stackla.com>
2014-07-09 22:27:25 +1000

    mobile fix

--- media/js/script2.js
+++ media/js/script2.js
@@ -16,6 +16,7 @@ window.addEventListener('deviceorientation', function(eventData) {
 
 
 $(document).ready(function() {
+	window.scrollTo(0, 0);
 	$('#fullpage').fullpage();
 	$('button').click(function() {
 		$.fn.fullpage.moveSectionDown();
