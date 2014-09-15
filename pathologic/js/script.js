a50ef04f2b9f39a522b336c84c4dfa57fc9b2ba2
Morgan Carter <morgancarter1@gmail.com>
2014-09-15 10:34:52 +1000

    lightbox, menu

--- pathologic/js/script.js
+++ pathologic/js/script.js
@@ -14,7 +14,27 @@ $(document).ready(function() {
         centerPadding: '40px',
     });
 
-    $('.js-toggleLanguage').click(function() {
-    	$('.js-toggleLanguage').toggleClass('active');
+    $('.js-language-toggler').click(function() {
+    	$('.js-language-toggler').toggleClass('active');
     });
+
+    $('.js-menu-toggler').click(function() {
+    	$('.js-menu-toggle').toggleClass('active');
+    });
+
+	$('.js-imagePopup').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
 });
