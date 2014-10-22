c38678b38444c1d64b5e220e8420f05020b6c6b2
Morgan Carter <morgancarter1@gmail.com>
2014-10-22 16:43:58 +1100

    maintenance

--- foot.php
+++
@@ -1,5 +0,0 @@
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script src='media/js/fastclick.js'></script>
-	<script src='media/js/script.js'></script>
-</body>
-</html>
