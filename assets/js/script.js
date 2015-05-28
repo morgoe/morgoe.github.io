b5f66aafdcaa88fcf25e6a4cecf821d3705fbbe0
Morgan Carter <morgancarter1@gmail.com>
2015-05-28 09:01:03 +0400

    GA fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,23 @@
 FastClick.attach(document.body);
 
 
+/* Google Analytics */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-63372069-1', 'auto');
+
+InstantClick.on('change', function() {
+	ga('send', 'pageview', location.pathname + location.search);
+});
+
+$(document).delegate('.js-cta-seeMyWork', 'click', function() {
+	ga('send', 'event', 'button', 'click', 'cta-seeMyWork');
+});
+
+
 /* Instant Click Initialise */
 InstantClick.init();
 
@@ -46,8 +63,3 @@ InstantClick.on('change', function() {
 /* Initial Animation */
 $('html').addClass('animateHeader--in');
 animateIn();
-
-/* Google Analytics */
-$('.js-cta-seeMyWork').on('click', function() {
-	ga('send', 'event', 'button', 'click', 'cta-seeMyWork');
-});
