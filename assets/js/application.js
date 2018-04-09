4dfd358d683b04b8e3018860e0984bb07e34bb37
Morgan Carter <morgancarter1@gmail.com>
2018-04-09 12:45:01 +1000

    It was case-sensitive

--- assets/js/application.js
+++ assets/js/application.js
@@ -6,4 +6,15 @@
 {% include_relative lib/sly.min.js %}
 {% include_relative lib/masonry.pkgd.min.js %}
 {% include_relative lib/autosize.min.js %}
-{% include_relative lib/instantClick.min.js %}
+{% include_relative lib/instantclick.min.js %}
+
+{% include_relative app/imageZoom.js %}
+{% include_relative app/videoScrubber.js %}
+{% include_relative app/carousel.js %}
+{% include_relative app/masonry.js %}
+{% include_relative app/contactForm.js %}
+{% include_relative app/autosizeTextarea.js %}
+{% include_relative app/externalLinks.js %}
+{% include_relative app/expandSvgs.js %}
+{% include_relative app/scrollTo.js %}
+{% include_relative app/instantClick.js %}
