c0e52c1b5377f9c946e0703cc98c616a66d8dded
Morgan Carter <morgancarter1@gmail.com>
2018-04-09 12:43:56 +1000

    that worked! add some more lines

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,3 +2,8 @@
 ---
 
 {% include_relative lib/medium-zoom.min.js %}
+{% include_relative lib/jquery.easing.js %}
+{% include_relative lib/sly.min.js %}
+{% include_relative lib/masonry.pkgd.min.js %}
+{% include_relative lib/autosize.min.js %}
+{% include_relative lib/instantClick.min.js %}
